2d7e22d715f3128a1398946f883509ebe9e04d6b
Rodrigo <rodrigo@hal>
2014-11-23 20:47:08 +0100

    Borrar p1 de la raíz.

--- Makefile
+++
@@ -1,6 +0,0 @@
-all: p1
-
-p1: p1.l
-	flex p1.l
-	gcc -g -lbsd -lfl lex.yy.c -o p1
-
--- do.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Compilando..."
-make
-echo
-echo "--------------- P1 --------------"
-cat test2.txt | ./p1
-echo "------------ Original -----------"
-cat -n test2.txt
